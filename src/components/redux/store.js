462a0a33a136fbdb98ce9ad89a998cfaee469006
Oleksii Donets <108811517+Davzp1980@users.noreply.github.com>
2024-08-19 17:06:28 +0300

    doing

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -18,7 +18,7 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'contacts/addContact': {
-      return { ...state, contacts: [...state.contacts, action.payload] };
+      return { ...state, contacts: [...state.contacts.items, action.payload] };
     }
     default:
       return state;
