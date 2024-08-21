c5e345e4f24492ea6756f1550560eb78c34517e4
Oleksii Donets <108811517+Davzp1980@users.noreply.github.com>
2024-08-21 16:17:39 +0300

    doing

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,23 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// export function setSearchValue(value) {
-//   return {
-//     type: 'filters/setSearchValue',
-//     payload: value,
-//   };
-// }
-
-// export const setSearchValue = createAction('filters/setSearchValue');
-
 const filterSlice = createSlice({
   name: 'filters',
   initialState: {
     name: '',
-
-    reducers: {
-      changeFilter(state, action) {
-        state.name = action.payload;
-      },
+  },
+  reducers: {
+    changeFilter(state, action) {
+      state.name = action.payload;
     },
   },
 });
